a708f99fde52832d5c05f97907dccf46640220e0
Manuel Jebauer <48769502+mjebauer@users.noreply.github.com>
2023-09-06 20:08:30 +0200

    Create PS_MainUserData.ps1

---
+++ PS_MainUserData.ps1
@@ -0,0 +1,24 @@
+$daten=(get-aduser -identity $args[0] -Properties * | Select Surname, GivenName, DisplayName, employeeType, SamAccountName, EmailAddress, UserPrincipalName, mailNickname, DistinguishedName, physicalDeliveryOfficeName, PasswordLastSet)
+
+# LastLogonDate
+# Description
+# PasswordLastSet
+# PasswordExpired
+
+#write-host "Nachname: " $daten.Surname
+#write-host "Vorname: " $daten.GivenName
+#write-host "Anzeigename: " $daten.DisplayName
+#write-host "Anmeldename: " $daten.SamAccountName
+#write-host "UPN: " $daten.UserPrincipalName
+#write-host "Mail-Nickname: " $daten.mailNickname
+
+$Output="Nachname: "+$daten.Surname+"`r`n"
+$Output+="Vorname: "+$daten.GivenName+"`r`n"
+$Output+="Anzeigename: "+$daten.DisplayName+"`r`n"
+$Output+="Anmeldename: "+$daten.SamAccountName+"`r`n"
+$Output+="UPN: "+$daten.UserPrincipalName+"`r`n"
+$Output+="OU: "+$daten.DistinguishedName+"`r`n"
+$Output+="Kostenstelle: "+$daten.physicalDeliveryOfficeName+"`r`n"
+$Output+="Email-Adresse: "+$daten.EmailAddress+"`r`n"
+$Output 
+$daten
