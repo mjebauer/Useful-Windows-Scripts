905788cbc083cc06418860cabbbfee3f60bd5af9
Manuel Jebauer <48769502+mjebauer@users.noreply.github.com>
2023-09-09 18:31:48 +0200

    Update PS_DomainJoin.ps1

--- PS_DomainJoin.ps1
+++ PS_DomainJoin.ps1
@@ -1,5 +1,8 @@
 #  Short script to join a domain with a client
 
+$domainStatus = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
+#  Short script to join a domain with a client
+
 function Join-Domain {
     $domain = "testadomini.tld" # Enter  domain here
     $puser = "TESTADOMINI\Administrator"  # Enter the domain and privileged user here
@@ -7,4 +10,18 @@ function Join-Domain {
 
 }
 
-Join-Domain
+if ($domainStatus) {
+    # Show warning message
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+    [System.Windows.Forms.MessageBox]::Show("The computer is already enrolled in the domain.", "Warnung", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
+    exit
+
+} else {
+    # Show warning message
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+    [System.Windows.Forms.MessageBox]::Show("The computer will be enrolled in the domain.", "Warnung", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
+    Join-Domain
+    
+}
